7aa9224ac16eccc2100f897af1c9fd41ed2168af
PINKDIAMONDVVS <chenjunyi531@gmail.com>
2024-02-14 16:55:53 -0500

    Initial Commit

---
+++ VisionProVREnv/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  VisionProVREnv
+//
+//  Created by Junyi Chen on 2/14/24.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ VisionProVREnv/VisionProVREnvApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionProVREnvApp.swift
+//  VisionProVREnv
+//
+//  Created by Junyi Chen on 2/14/24.
+//
+
+import SwiftUI
+
+@main
+struct VisionProVREnvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
