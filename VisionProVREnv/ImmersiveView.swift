c017c7a81ae8d9156d7fdb2a8b3dcecb052d0685
PINKDIAMONDVVS <chenjunyi531@gmail.com>
2024-02-14 18:08:13 -0500

    commit 2

---
+++ VisionProVREnv/ImmersiveView.swift
@@ -0,0 +1,18 @@
+//
+//  ImmersiveView.swift
+//  VisionProVREnv
+//
+//  Created by Junyi Chen on 2/14/24.
+//
+
+import SwiftUI
+
+struct ImmersiveView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImmersiveView()
+}
